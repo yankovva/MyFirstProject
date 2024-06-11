32e9459909f3ea5e1af6735eaabadba6f54d60f8
yankovva <maggie.yankovaaa@gmail.com>
2024-06-11 16:59:41 +0300

    Add files via upload

---
+++ Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-S3FD0JI\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+    }
+}
